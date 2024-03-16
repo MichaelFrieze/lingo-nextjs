b7fb297be3db51879e241a964f43ba7b2f4834a1
Michael Frieze <michaelfrieze@icloud.com>
2024-03-15 20:19:17 -0400

    feat: add marketing page and add all project assets in the public folder

---
+++ app/(marketing)/layout.tsx
@@ -0,0 +1,17 @@
+type Props = {
+  children: React.ReactNode;
+};
+
+const MarketingLayout = ({ children }: Props) => {
+  return (
+    <div className="flex min-h-screen flex-col">
+      {/* <Header /> */}
+      <main className="flex flex-1 flex-col items-center justify-center">
+        {children}
+      </main>
+      {/* <Footer /> */}
+    </div>
+  );
+};
+
+export default MarketingLayout;
--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Button } from "@/components/ui/button";
-
-export default function Home() {
-  return (
-    <div className="font-bold text-green-500">
-      <h1>Home Page</h1>
-    </div>
-  );
-}
