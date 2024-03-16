c7d4ede782156eda3564801462ef19e7988a3775
Michael Frieze <michaelfrieze@icloud.com>
2024-03-15 20:12:49 -0400

    feat: add buttons library

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div className="font-bold text-green-500">
-      <Button size="lg" variant="destructive">
-        Click Me
-      </Button>
+      <h1>Home Page</h1>
     </div>
   );
 }
